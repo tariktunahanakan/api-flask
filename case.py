8ed19f3a91e76e7de0ca79509ef7f132baef565b
tariktunahanakan <akantariktunahan@gmail.com>
2022-02-05 06:48:03 +0300

    My first commit message

---
+++ case.py
@@ -0,0 +1,29 @@
+from urllib import request
+import flask
+import json, time
+import requests, json
+
+app = flask.Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    name_surname = {'name': 'Tarik Tunahan', 'surname': 'Akan'}
+    home_page = json.dumps(name_surname)
+    return home_page
+
+@app.route('/temperature&city=<city>', methods=['GET'])
+def temperature_page(city):
+    apikey = "9876fd2d4b8a4a6ba198bde135f43c98"     
+    url = f'https://api.weatherbit.io/v2.0/current?&city={city}&key={apikey}&include=minutely'
+    response = requests.get(url)
+    data = response.json()['data'][0]
+    temp = data['temp']
+    print(f'Degrees: {temp}')
+
+    temperature_city = {'temperature': temp}
+    temperature_page = json.dumps(temperature_city)
+    return temperature_page
+
+   
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
